6593d2075bce9489efb31e1e6b36c95b4e52976e
ramyres110 <ramyres90@gmail.com>
2020-09-18 15:17:37 -0300

    Alteracao no build do app

---
+++ public/precache-manifest.1879ab898b8625685af40ef6ad989a6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c015a17afbdd76a38ac206f1eea9b52d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d87cbdc321024a461a92",
+    "url": "/static/js/2.fbfb128c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fbfb128c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be99cb23ce7744d7b525",
+    "url": "/static/js/main.20563fff.chunk.js"
+  },
+  {
+    "revision": "ec9875811aa0f00c2e31",
+    "url": "/static/js/runtime-main.92ee656d.js"
+  }
+]);
